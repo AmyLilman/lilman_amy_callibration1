9900270f4fc349aec2acedd7921c5dc58b2ee376
AmyLilman <72803028+AmyLilman@users.noreply.github.com>
2021-02-07 19:22:08 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #light
-print(input.light_level())
+#while True:
+#    print(input.light_level())
+#    if (input.light_level()) > 0:
+#        light.clear()
+#    else:
+#        light.clear()
+
+print(input.light_level() + 35.6)
+
+while True: 
+   if input.light_level()+ 35.6 > 400:
+       music.power_up.play_until_done()
--- main.ts
+++ main.ts
@@ -1,2 +1,14 @@
 // light
-console.log(input.lightLevel())
+// while True:
+//     print(input.light_level())
+//     if (input.light_level()) > 0:
+//         light.clear()
+//     else:
+//         light.clear()
+console.log(input.lightLevel() + 35.6)
+while (true) {
+    if (input.lightLevel() + 35.6 > 400) {
+        music.powerUp.playUntilDone()
+    }
+    
+}
